9c9d97f7647045d0030fb7797b82bcad0c4bb168
Jetalir <Jetalir@hotmail.com>
2024-10-17 10:05:55 +0200

    Add project files.

---
+++ FitTrack/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MVVM_Genomgång.Model;
+using MVVM_Genomgång.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.ViewModel
+{
+    internal class MainWindowViewModel
+    {
+        
+        
+    }
+}
